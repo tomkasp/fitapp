ced4c66fc48a42278af24ac151f4f6cd26ba053c
tomkasp@gmail.com <tomkasp@gmail.com>
2017-06-10 21:49:50 +0200

    training continue

--- src/main/java/com/tomkasp/training/domain/trainingplan/FiveKPlanFactory.java
+++ src/main/java/com/tomkasp/training/domain/trainingplan/FiveKPlanFactory.java
@@ -15,8 +15,31 @@ public class FiveKPlanFactory {
         List<String> trainingDays = new ArrayList<>();
         String firstDay = "free";
         String secondDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String thirdDay = "free";
+        String fourthDay = "10min run with tempo " + runTempos.easy();
+        String fifthDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String sixthDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String seventhDAy = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String eightDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String ninthDat = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String tenthDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String eleventhDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String twelveDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String thirteenDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String fourtennDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+        String fifteenthDay = "15min run with tempo" + runTempos.easy() + "15min run with tempo:" + runTempos.longRunMax();
+
         trainingDays.add(firstDay);
         trainingDays.add(secondDay);
+        trainingDays.add(thirdDay);
+        trainingDays.add(fourthDay);
+        trainingDays.add(fifthDay);
+        trainingDays.add(sixthDay);
+        trainingDays.add(seventhDAy);
+        trainingDays.add(eightDay);
+        trainingDays.add(ninthDat);
+        trainingDays.add(tenthDay);
+        trainingDays.add(eleventhDay);
 
     }
 
